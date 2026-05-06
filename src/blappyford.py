d5236e505198faa6757f4d66a710b331f6eeeb49
Nicholas Camp <xraymonster8@gmail.com>
2026-05-06 12:54:24 -0500

    Additional pygame skeletal structure

--- src/blappyford.py
+++ src/blappyford.py
@@ -4,7 +4,17 @@ import random
 
 def main():
     pygame.init()
-    
+    pygame.display.set_caption("Adventures of Blappyford")
+    screen = pygame.display.set_mode((800,600))
+    clock = pygame.time.Clock()
+    font = pygame.font.Font(None,24)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
 
 if __name__ == "__main__":
     main()
