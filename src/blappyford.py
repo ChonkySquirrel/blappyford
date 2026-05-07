5d18baa6d1a65656769bad95dc97688886dc4029
Nicholas Camp <xraymonster8@gmail.com>
2026-05-07 13:49:50 -0500

    added a wallpair object, which should make and connect two active wall objects with random heights that don't intersect each other
    changed testing and wall object variables to fit with the new change

--- src/blappyford.py
+++ src/blappyford.py
@@ -8,11 +8,11 @@ WIDTH = 1080
 HEIGHT = 720
 SCREEN_COLOR = (0,0,0)
 
-class BottomWall():
-    def __init__(self):
-        self.height = 200
+class Wall():
+    def __init__(self, height, altitude):
+        self.height = height
         self.speed = 100
-        self.pos = ([WIDTH,HEIGHT-self.height])
+        self.pos = ([WIDTH,altitude])
         self._rect = pygame.Rect(self.pos[0],self.pos[1],WIDTH/20,self.height)
 
     def update (self,dt):
@@ -26,6 +26,26 @@ class BottomWall():
     def draw(self,surface):
         pygame.draw.rect(surface,(255,0,0),self._rect)
 
+class WallPair():
+    def __init__(self):
+        self.topheight = random.randrange(100,500)
+        self.botheight = HEIGHT
+        while (HEIGHT-self.botheight > self.topheight + 60):
+            self.botheight = random.randrange(100,500)
+        self.walls = []
+        self._makewallpair()
+
+    def _makewallpair(self):
+        self.walls.append(Wall(self.topheight,0))
+        self.walls.append(Wall(self.botheight,HEIGHT-self.botheight))
+    
+    def update(self,dt):
+        for wall in self.walls():
+            wall.update(dt)
+    
+    def draw(self,surface):
+        for wall in self.walls():
+            wall.draw(surface)
 
 def main():
     pygame.init()
@@ -34,7 +54,7 @@ def main():
     clock = pygame.time.Clock()
     font = pygame.font.Font(None,24)
     dt = 0
-    wall = BottomWall()
+    walls = WallPair()
 
     running = True
     while running:
@@ -43,10 +63,10 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         # Game Logic
-        wall.update(dt)
+        walls.update(dt)
         # Render & Display
         screen.fill(SCREEN_COLOR)
-        wall.draw(screen)
+        walls.draw(screen)
         pygame.display.flip()
         dt = clock.tick(30)/1000.0
 
